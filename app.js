6fd46381adfe4e8fe51fac49c52da5c166db9c68
ElliottMcDonald <E.mcdonald657@gmail.com>
2024-11-02 15:15:08 +0000

    Install and import Mongoose. Configure setup
    Establish mongoose set-up using a promise. Place app.listen command for express server within then block. Sever will only start once database is connected.
    Display console.log messages to confirm server and database are running.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Import the Mongoose library
+const mongoose = require("mongoose");
+
 //Import the express module.
 const express = require("express");
 const cors = require("cors");
@@ -9,10 +12,28 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //------------------Middleware-------------------//
-//Allow requets from different origins.
+//Allow requests from different origins.
 app.use(cors());
 //Displays static material, a homepage when no URL extension added.
 app.use(express.static("public"));
 //Makes incoming data request from client readable by server.
 //Turns JSON into Javascript Objects.
 app.use(express.json());
+
+//--------------------------------------------------------//
+// -------------Mongoose Set-up---------------------------//
+//--------------------------------------------------------//
+
+// Establish a connection to database at "mongodb://localhost/databaseName".
+// The .connect() method returns a promise that resolves when the connection is successful.
+mongoose
+  .connect("mongodb://localhost/students") // Specify the connection string for the MongoDB database.
+  .then(() => {
+    console.log("Connected to MongoDB");
+    // Start the server only after the mongodb connection is established.
+    //This prevents errors/problems caused by not having access to the database.
+    //When making requests.
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  });
