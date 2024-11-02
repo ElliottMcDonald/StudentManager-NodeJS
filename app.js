938896a6fcec57cc5aa1282fc058008d4f9dac50
ElliottMcDonald <E.mcdonald657@gmail.com>
2024-11-02 15:17:19 +0000

    Add catch block to catch errors.

--- app.js
+++ app.js
@@ -36,4 +36,8 @@ mongoose
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
+  })
+  //Catch block to display error in server or database connection.
+  .catch((error) => {
+    console.error(`Error: ${error}`);
   });
